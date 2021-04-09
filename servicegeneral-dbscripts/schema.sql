cadea1d637a6b0b02ba75a778290b87a29a46fd0
Melba Moro <melba.moro17@gmail.com>
2021-04-08 22:10:46 -0400

    feedback functionality added

--- servicegeneral-dbscripts/schema.sql
+++ servicegeneral-dbscripts/schema.sql
@@ -9,4 +9,12 @@ CREATE TABLE `user` (
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-SELECT * FROM `service-general`.user;
+
+
+
+CREATE TABLE `feedback` (
+  `feedbackid` bigint NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`feedbackid`)
+);
